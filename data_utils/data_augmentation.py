6b22b89eb4f7f1b9ceba5b1fcc69065f666f0756
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2022-09-23 14:14:03 +0700

    Update data_augmentation.py

--- data_utils/data_augmentation.py
+++ data_utils/data_augmentation.py
@@ -2,11 +2,11 @@ from augmenter.augmentation import *
 from configs import base_config as cfg
 
 class Augmentor():
-    def __init__(self, target_size=(416, 416, 3), max_bboxes_per_scale=cfg.YOLO_MAX_BBOX_PER_SCALE, aug_mode='train'):
+    def __init__(self, target_size=(416, 416, 3), max_bboxes=100, aug_mode='train'):
         if aug_mode == 'train':
-            self.aug = Augment2(target_size=target_size, max_boxes=max_bboxes_per_scale)
+            self.aug = Augment2(target_size=target_size, max_boxes=max_bboxes)
         else:            
-            self.aug = Augment1(target_size=target_size, max_boxes=max_bboxes_per_scale)
+            self.aug = Augment1(target_size=target_size, max_boxes=max_bboxes)
 
         self.sequence = [self.aug]
 
