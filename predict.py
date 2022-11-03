4d5febe14e3584c64d1126908e5af6ace968149d
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2022-11-03 09:52:47 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 from models.yolo import YOLO
 from models.yolov3 import YOLOv3Encoder, YOLOv3Decoder
 from models.architectures.darknet53 import DarkNet53
-from utils.post_processing import get_label_name
+from utils.post_processing import get_labels
 from visualizer.visual_image import visual_image, visual_image_with_bboxes
 from configs import base_config as cfg
 
@@ -83,7 +83,7 @@ def detect_image(img_name, model, target_shape, class_names, crop=False, count=F
 
 
     for i, c in list(enumerate(out_classes)):
-        predicted_class = get_label_name(class_names, int(c))
+        predicted_class = class_names[int(c)]
         box             = out_boxes[i]
         score           = out_scores[i]
 
@@ -109,17 +109,7 @@ def detect_image(img_name, model, target_shape, class_names, crop=False, count=F
     return image
 
 
-classes = {'person': 1, 'bicycle': 2, 'car': 3, 'motorcycle': 4, 'airplane': 5, 'bus': 6, 'train': 7, 'truck': 8,
-                      'boat': 9, 'traffic light': 10, 'fire hydrant': 11, 'stop sign': 12, 'parking meter': 13, 'bench': 14, 'bird': 15, 'cat': 16,
-                      'dog': 17, 'horse': 18, 'sheep': 19, 'cow': 20, 'elephant': 21, 'bear': 22, 'zebra': 23, 'giraffe': 24,
-                      'backpack': 25, 'umbrella': 26, 'handbag': 27, 'tie': 28, 'suitcase': 29, 'frisbee': 30, 'skis': 31, 'snowboard': 32,
-                      'sports ball': 33, 'kite': 34, 'baseball bat': 35, 'baseball glove': 36, 'skateboard': 37, 'surfboard': 38, 'tennis racket': 39, 'bottle': 40,
-                      'wine glass': 41, 'cup': 42, 'fork': 43, 'knife': 44, 'spoon': 45, 'bowl': 46, 'banana': 47, 'apple': 48,
-                      'sandwich': 49, 'orange': 50, 'broccoli': 51, 'carrot': 52, 'hot dog': 53, 'pizza': 54, 'donut': 55, 'cake': 56,
-                      'chair': 57, 'couch': 58, 'potted plant': 59, 'bed': 60, 'dining table': 61, 'toilet': 62, 'tv': 63, 'laptop': 64,
-                      'mouse': 65, 'remote': 66, 'keyboard': 67, 'cell phone': 68, 'microwave': 69, 'oven': 70, 'toaster': 71, 'sink': 72,
-                      'refrigerator': 73, 'book': 74, 'clock': 75, 'vase': 76, 'scissors': 77,  'teddy bear': 78, 'hair drier': 79, 'toothbrush': 80}
-num_classes = len(classes)
+classes, num_classes = get_labels(cfg.YOLO_CLASSES_FILE)
 
 backbone = DarkNet53(input_shape   = cfg.YOLO_TARGET_SIZE, 
                      activation    = cfg.YOLO_BACKBONE_ACTIVATION, 
@@ -136,7 +126,7 @@ decoder = YOLOv3Decoder(anchors     = cfg.YOLO_ANCHORS,
                         input_size  = cfg.YOLO_TARGET_SIZE,
                         anchor_mask = cfg.YOLO_ANCHORS_MASK,
                         max_boxes   = cfg.YOLO_MAX_BBOXES,
-                        confidence  = cfg.TEST_CONFIDENCE_THRESHOLD,
+                        confidence  = 0.5,
                         nms_iou     = cfg.TEST_IOU_THRESHOLD,
                         letterbox_image=True)
 
@@ -163,6 +153,6 @@ img = detect_image(image,
                    model, 
                    cfg.YOLO_TARGET_SIZE, 
                    classes, 
-                   crop=True, 
+                   crop=False, 
                    count=False, 
                    letterbox_image=True)
