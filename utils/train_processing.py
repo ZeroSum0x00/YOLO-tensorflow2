42db755253969e309b31337423fa6af3008c3ab2
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2022-12-31 09:38:49 +0700

    Update train_processing.py

--- utils/train_processing.py
+++ utils/train_processing.py
@@ -1,9 +1,31 @@
 import os
+import logging
 import datetime
+import tensorflow as tf
 from utils.logger import logger
 from utils.files import verify_folder
 
-
+def train_prepare(train_mode):
+    try:
+        logger.info(f"Setting trainer width {train_mode.lower()} mode")
+        if train_mode.lower() == 'eager':
+            os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+            tf.get_logger().setLevel(logging.ERROR)
+            tf.config.run_functions_eagerly(True)
+            return True
+        elif train_mode.lower() == 'graph':
+            os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+            tf.get_logger().setLevel(logging.ERROR)
+            tf.config.run_functions_eagerly(False)
+            return True
+        else:
+            logger.error(f"Can't find {train_mode} mode. You only choose 'eager' or 'graph'")
+            return False
+    except BaseException as e:
+        print(e)
+        return False
+    
+    
 def create_folder_weights(saved_dir):
     current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     TRAINING_TIME_PATH = saved_dir + current_time
