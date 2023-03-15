04f6a0a4d28f0e3a85fbfb85a6b56ef79b729954
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-03-15 21:21:39 +0700

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -11,14 +11,11 @@ from utils.logger import logger
 
 class YOLO(tf.keras.Model):
     def __init__(self, 
-                 encoder, 
-                 decoder, 
+                 architecture, 
                  image_size=(416, 416, 3), 
-                 is_training=False, 
                  **kwargs):
         super(YOLO, self).__init__(**kwargs)
-        self.encoder = encoder
-        self.decoder = decoder
+        self.architecture = architecture
         self.image_size = image_size
         self.total_loss_tracker = tf.keras.metrics.Mean(name="total_loss")
 
@@ -37,11 +34,11 @@ class YOLO(tf.keras.Model):
         images, targets = data
         targets = [targets[2], targets[1], targets[0]]
         with tf.GradientTape() as tape:
-            y_pred      = self.encoder(images, training=True)
+            y_pred      = self.architecture(images, training=True)
             loss_value  = self.yolo_loss(y_true=targets, y_pred=y_pred)
-            loss_value  = tf.reduce_sum(self.encoder.losses) + loss_value
-        grads = tape.gradient(loss_value, self.encoder.trainable_variables)
-        self.optimizer.apply_gradients(zip(grads, self.encoder.trainable_variables))
+            loss_value  = tf.reduce_sum(self.architecture.losses) + loss_value
+        grads = tape.gradient(loss_value, self.architecture.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.architecture.trainable_variables))
 
         self.total_loss_tracker.update_state(loss_value)
         results = {m.name: m.result() for m in self.metrics}
@@ -51,9 +48,9 @@ class YOLO(tf.keras.Model):
     def test_step(self, data):
         images, targets = data
         targets = [targets[2], targets[1], targets[0]]
-        y_pred      = self.encoder(images, training=False)
+        y_pred      = self.architecture(images, training=False)
         loss_value  = self.yolo_loss(y_true=targets, y_pred=y_pred)
-        loss_value  = tf.reduce_sum(self.encoder.losses) + loss_value
+        loss_value  = tf.reduce_sum(self.architecture.losses) + loss_value
         self.total_loss_tracker.update_state(loss_value)
         results = {m.name: m.result() for m in self.metrics}
         return results
@@ -68,16 +65,16 @@ class YOLO(tf.keras.Model):
     @tf.function
     def predict(self, inputs):
         images, origin_shape = inputs
-        output_encoder = self.encoder(images, training=False)
-        inputs  = [*output_encoder, origin_shape]
-        out_boxes, out_scores, out_classes = self.decoder(inputs)
+        output_model = self.architecture(images, training=False)
+        inputs  = [*output_model, origin_shape]
+        out_boxes, out_scores, out_classes = self.architecture.decode(inputs)
         return out_boxes, out_scores, out_classes
 
     def save_weights(self, weight_path, save_head=True, save_format='tf', **kwargs):
         if save_head:
-            self.encoder.save_weights(weight_path, save_format=save_format, **kwargs)
+            self.architecture.save_weights(weight_path, save_format=save_format, **kwargs)
         else:
-            backup_model = Model(inputs=self.encoder.input, outputs=self.encoder.output)
+            backup_model = Model(inputs=self.architecture.input, outputs=self.architecture.output)
             backup_model.get_layer("medium_bbox_predictor").pop()
             backup_model.get_layer("large_bbox_predictor").pop()
             backup_model.get_layer("small_bbox_predictor").pop()
@@ -88,27 +85,26 @@ class YOLO(tf.keras.Model):
             weight_path = weight['path']
             custom_objects = weight['custom_objects']
             if weight_path:
-                self.encoder.build(input_shape=self.image_size)
-                self.encoder.built = True
-                self.encoder.load_weights(weight_path)
+                self.architecture.build(input_shape=self.image_size)
+                self.architecture.built = True
+                self.architecture.load_weights(weight_path)
                 logger.info("Load yolo weights from {}".format(weight_path))
 
     def save_models(self, weight_path, save_format='tf'):
-        self.encoder.save(weight_path, save_format=save_format)
+        self.architecture.save(weight_path, save_format=save_format)
 
     def load_models(self, weight_objects):
         for weight in weight_objects:
             weight_path = weight['path']
             custom_objects = weight['custom_objects']
             if weight_path:
-                self.encoder = load_model(weight_path, custom_objects=custom_objects)
+                self.architecture = load_model(weight_path, custom_objects=custom_objects)
                 logger.info("Load yolo model from {}".format(weight_path))
 
     def get_config(self):
         config = super().get_config()
         config.update({
-                "encoder": self.encoder,
-                "decoder": self.decoder,
+                "architecture": self.architecture,
                 "total_loss_tracker": self.total_loss_tracker,
                 "optimizer": self.optimizer
             })
