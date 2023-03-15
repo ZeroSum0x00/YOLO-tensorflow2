95c6116fdba1bf63b3f325b13ef849e7b95b007a
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-03-15 13:25:39 +0700

    Update general_config.py

--- configs/general_config.py
+++ configs/general_config.py
@@ -32,7 +32,7 @@ YOLO_ANCHORS                    = [[ 10,  13],
                                    [156, 198],
                                    [373, 326]]
 
-YOLO_ANCHORS_MASK               = [[6, 7, 8], [3, 4, 5], [0, 1, 2]]
+YOLO_ANCHORS_MASK               = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
 YOLO_STRIDES                    = [8, 16, 32]
 
