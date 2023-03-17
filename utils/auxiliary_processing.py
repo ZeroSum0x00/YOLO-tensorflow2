957b4644afb7492b759801f4cc4475e2bea0ffbd
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-03-18 01:11:07 +0700

    Update auxiliary_processing.py

--- utils/auxiliary_processing.py
+++ utils/auxiliary_processing.py
@@ -1,5 +1,19 @@
+import cv2
 import numpy as np
 
 
 def random_range(a=0, b=1):
     return np.random.rand() * (b - a) + a
+
+
+def change_color_space(image, current_space="BGR", to_space="BGR"):
+    if not ((current_space.lower() in {'bgr', 'rgb', 'hsv'}) 
+            and (to_space.lower() in {'bgr', 'rgb', 'hsv', 'gray'})):
+        raise NotImplementedError
+    if current_space.lower() == 'bgr' and to_space.lower() == 'rgb':
+        return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    if current_space.lower() == 'rgb' and to_space.lower() == 'bgr':
+        return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    elif current_space.lower() == 'bgr' and to_space.lower() == 'hsv':
+        return cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    return image
