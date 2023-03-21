4ead084292eb44edab612df73880c40ca4b742df
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-03-21 23:55:25 +0700

    Update auxiliary_processing.py

--- utils/auxiliary_processing.py
+++ utils/auxiliary_processing.py
@@ -17,3 +17,17 @@ def change_color_space(image, current_space="BGR", to_space="BGR"):
     elif current_space.lower() == 'bgr' and to_space.lower() == 'hsv':
         return cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     return image
+
+
+def coordinates_converter(bboxes, conversion=None):
+    if conversion == "centroids2corners":
+        boxes_xy = bboxes[..., 0:2] - 0.5 * bboxes[..., 2:4]
+        boxes_wh = bboxes[..., 0:2] + 0.5 * bboxes[..., 2:4]
+        bboxes[..., 0:2] = boxes_xy
+        bboxes[..., 2:4] = boxes_wh
+    elif conversion == "corners2centroids":
+        boxes_xy  = (bboxes[..., 0:2] + bboxes[..., 2:4]) // 2
+        boxes_wh = bboxes[..., 2:4] - bboxes[..., 0:2]
+        bboxes[..., 0:2] = boxes_xy
+        bboxes[..., 2:4] = boxes_wh
+    return bboxes
