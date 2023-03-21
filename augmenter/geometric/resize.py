6b10ce33f3978411aa8178e5b1bf179c5b08e1a8
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-03-21 23:54:43 +0700

    Update resize.py

--- augmenter/geometric/resize.py
+++ augmenter/geometric/resize.py
@@ -2,20 +2,20 @@ import cv2
 import random
 import numpy as np
 
-from utils.auxiliary_processing import random_range
+from utils.auxiliary_processing import random_range, coordinates_converter
 from visualizer.visual_image import visual_image, visual_image_with_bboxes
 
 
 class Resize:
-    def __init__(self, target_size=(416, 416, 3), max_bboxes=500, interpolation=None):
+    def __init__(self, target_size=(416, 416, 3), coords="corners", max_bboxes=500, interpolation=None):
         self.target_size   = target_size
+        self.coords        = coords
         self.max_bboxes    = max_bboxes
         self.interpolation = interpolation
 
     def __call__(self, image, bboxes):
         h, w, _    = image.shape
         ih, iw, _  = self.target_size
-        print(ih)
         image = cv2.resize(image, dsize=(iw, ih), interpolation=self.interpolation)
         scale = min(iw/w, ih/h)
         nw, nh  = int(scale * w), int(scale * h)
@@ -23,9 +23,23 @@ class Resize:
         box_data = np.zeros((self.max_bboxes, 5))
         if len(bboxes) > 0:
             np.random.shuffle(bboxes)
+            if self.coords == "centroids":
+                bboxes[..., 0] = bboxes[..., 0] * w
+                bboxes[..., 1] = bboxes[..., 1] * h
+                bboxes[..., 2] = bboxes[..., 2] * w
+                bboxes[..., 3] = bboxes[..., 3] * h
+                bboxes = coordinates_converter(bboxes, conversion="centroids2corners")
+
             bboxes[:, [0, 2]] = np.round(bboxes[:, [0, 2]] * (iw/w), decimals=0)
             bboxes[:, [1, 3]] = np.round(bboxes[:, [1, 3]] * (ih/h), decimals=0)
             bboxes[:, 0:2][bboxes[:, 0:2] < 0] = 0
+            if self.coords == "centroids":
+                bboxes = coordinates_converter(bboxes, conversion="corners2centroids")
+                bboxes[..., 0] = bboxes[..., 0] / iw
+                bboxes[..., 1] = bboxes[..., 1] / ih
+                bboxes[..., 2] = bboxes[..., 2] / iw
+                bboxes[..., 3] = bboxes[..., 3] / ih
+                
             if len(bboxes) > self.max_bboxes: 
                 bboxes = bboxes[:self.max_bboxes]
 
@@ -34,106 +48,131 @@ class Resize:
       
 
 class ResizePadded:
-    def __init__(self, target_size=(416, 416, 3), max_boxes=10, jitter=.3, flexible=False):
+    def __init__(self, target_size=(416, 416, 3), coords="corners", max_boxes=10, jitter=.3, flexible=False):
         self.target_size = target_size
-        self.max_boxes = max_boxes
-        self.jitter    = jitter
-        self.flexible      = flexible
+        self.coords      = coords
+        self.max_boxes   = max_boxes
+        self.jitter      = jitter
+        self.flexible    = flexible
 
     def __call__(self, image, bboxes):
-      h, w, _    = image.shape
-      ih, iw, _  = self.target_size
+        h, w, _    = image.shape
+        ih, iw, _  = self.target_size
 
-      if not self.flexible:
-          scale = min(iw/w, ih/h)
-          nw, nh  = int(scale * w), int(scale * h)
-          dw, dh = (iw - nw) // 2, (ih - nh) // 2
-          image_resized = cv2.resize(image, (nw, nh))
-          image_paded = np.full(shape=[ih, iw, 3], fill_value=128.0, dtype=image.dtype)
-          image_paded[dh:nh+dh, dw:nw+dw, :] = image_resized
+        if not self.flexible:
+            scale = min(iw/w, ih/h)
+            nw, nh  = int(scale * w), int(scale * h)
+            dw, dh = (iw - nw) // 2, (ih - nh) // 2
+            image_resized = cv2.resize(image, (nw, nh))
+            image_paded = np.full(shape=[ih, iw, 3], fill_value=128.0, dtype=image.dtype)
+            image_paded[dh:nh+dh, dw:nw+dw, :] = image_resized
 
-          box_data = np.zeros((self.max_boxes, 5))
-          if len(bboxes) > 0:
-              np.random.shuffle(bboxes)
-              bboxes[:, [0, 2]] = bboxes[:, [0, 2]] * scale + dw
-              bboxes[:, [1, 3]] = bboxes[:, [1, 3]] * scale + dh
-              bboxes[:, 0:2][bboxes[:, 0:2] < 0]   = 0
-              bboxes[:, 2][bboxes[:, 2] > iw]      = iw
-              bboxes[:, 3][bboxes[:, 3] > ih]      = ih
-              box_w   = bboxes[:, 2] - bboxes[:, 0]
-              box_h   = bboxes[:, 3] - bboxes[:, 1]
-              bboxes  = bboxes[np.logical_and(box_w > 1, box_h > 1)]
-              if len(bboxes) > self.max_boxes: bboxes = bboxes[:self.max_boxes]
-              box_data[:len(bboxes)] = bboxes
-          return image_paded, box_data
+            box_data = np.zeros((self.max_boxes, 5))
+            if len(bboxes) > 0:
+                np.random.shuffle(bboxes)
+                if self.coords == "centroids":
+                    bboxes[..., 0] = bboxes[..., 0] * w
+                    bboxes[..., 1] = bboxes[..., 1] * h
+                    bboxes[..., 2] = bboxes[..., 2] * w
+                    bboxes[..., 3] = bboxes[..., 3] * h
+                    bboxes = coordinates_converter(bboxes, conversion="centroids2corners")
+                
+                bboxes[:, [0, 2]] = bboxes[:, [0, 2]] * scale + dw
+                bboxes[:, [1, 3]] = bboxes[:, [1, 3]] * scale + dh
+                bboxes[:, 0:2][bboxes[:, 0:2] < 0]   = 0
+                bboxes[:, 2][bboxes[:, 2] > iw]      = iw
+                bboxes[:, 3][bboxes[:, 3] > ih]      = ih
+                box_w   = bboxes[:, 2] - bboxes[:, 0]
+                box_h   = bboxes[:, 3] - bboxes[:, 1]
+                bboxes  = bboxes[np.logical_and(box_w > 1, box_h > 1)]
+                if self.coords == "centroids":                    
+                    bboxes = coordinates_converter(bboxes, conversion="corners2centroids")
+                    bboxes[..., 0] = bboxes[..., 0] / iw
+                    bboxes[..., 1] = bboxes[..., 1] / ih
+                    bboxes[..., 2] = bboxes[..., 2] / iw
+                    bboxes[..., 3] = bboxes[..., 3] / ih
+                if len(bboxes) > self.max_boxes: 
+                    bboxes = bboxes[:self.max_boxes]
+                box_data[:len(bboxes)] = bboxes
+            return image_paded, box_data
 
-      new_ar = w / h * random_range(1 - self.jitter, 1 + self.jitter) / random_range(1 - self.jitter, 1 + self.jitter)
-      scale = random_range(.25, 2)
+        new_ar = w / h * random_range(1 - self.jitter, 1 + self.jitter) / random_range(1 - self.jitter, 1 + self.jitter)
+        scale = random_range(.25, 2)
 
-      if new_ar < 1:
-          nh = int(scale * ih)
-          nw = int(nh * new_ar)
-      else:
-          nw = int(scale * iw)
-          nh = int(nw / new_ar)
+        if new_ar < 1:
+            nh = int(scale * ih)
+            nw = int(nh * new_ar)
+        else:
+            nw = int(scale * iw)
+            nh = int(nw / new_ar)
           
-      dw = int(random_range(0, iw - nw))
-      dh = int(random_range(0, ih - nh))
+        dw = int(random_range(0, iw - nw))
+        dh = int(random_range(0, ih - nh))
 
-      image_resized = cv2.resize(image, (nw, nh))
+        image_resized = cv2.resize(image, (nw, nh))
 
-      height = max(ih, nh + abs(dh))
-      width = max(iw, nw + abs(dw))
-      image_paded = np.full(shape=[height, width, 3], fill_value=128.0)
-      if dw < 0 and dh >= 0:
-          image_paded[dh:nh+dh, 0:nw, :] = image_resized
-          if width == iw:
-              image_paded = image_paded[:ih, :iw]
-          else:
-              image_paded = image_paded[:ih, abs(dw):abs(dw)+iw]
-      elif dh < 0 and dw >= 0:
-          image_paded[0:nh, dw:dw+nw, :] = image_resized
-          if height == ih:
-              image_paded = image_paded[:ih, :iw]
-          else:
-              image_paded = image_paded[abs(dh):abs(dh)+ih, :iw]
-      elif dh < 0 and dw < 0:
-          image_paded[0:nh, 0:nw, :] = image_resized
-          if width == iw or height == ih:
-              image_paded = image_paded[:ih, :iw]
-          else:
-              image_paded = image_paded[abs(dh):abs(dh)+ih, abs(dw):abs(dw)+iw]
-      else:
-          image_paded[dh:nh+dh, dw:dw+nw, :] = image_resized
-          image_paded = image_paded[:ih, :iw]
+        height = max(ih, nh + abs(dh))
+        width = max(iw, nw + abs(dw))
+        image_paded = np.full(shape=[height, width, 3], fill_value=128.0)
+        if dw < 0 and dh >= 0:
+            image_paded[dh:nh+dh, 0:nw, :] = image_resized
+            if width == iw:
+                image_paded = image_paded[:ih, :iw]
+            else:
+                image_paded = image_paded[:ih, abs(dw):abs(dw)+iw]
+        elif dh < 0 and dw >= 0:
+            image_paded[0:nh, dw:dw+nw, :] = image_resized
+            if height == ih:
+                image_paded = image_paded[:ih, :iw]
+            else:
+                image_paded = image_paded[abs(dh):abs(dh)+ih, :iw]
+        elif dh < 0 and dw < 0:
+            image_paded[0:nh, 0:nw, :] = image_resized
+            if width == iw or height == ih:
+                image_paded = image_paded[:ih, :iw]
+            else:
+                image_paded = image_paded[abs(dh):abs(dh)+ih, abs(dw):abs(dw)+iw]
+        else:
+            image_paded[dh:nh+dh, dw:dw+nw, :] = image_resized
+            image_paded = image_paded[:ih, :iw]
 
-      hpd, wpd, _ = image_paded.shape
-      if hpd < ih or wpd <iw:
-          image_temp = np.full(shape=[ih, iw, 3], fill_value=128.0)
-          image_temp[:hpd, :wpd] = image_paded
-          image_paded = image_temp
+        hpd, wpd, _ = image_paded.shape
+        if hpd < ih or wpd <iw:
+            image_temp = np.full(shape=[ih, iw, 3], fill_value=128.0)
+            image_temp[:hpd, :wpd] = image_paded
+            image_paded = image_temp
 
-      image = image_paded
-      image_data      = np.array(image, np.uint8)
+        image = image_paded
+        image_data      = np.array(image, np.uint8)
 
-      box_data = np.zeros((self.max_boxes, 5))
-      if len(bboxes) > 0:
-          np.random.shuffle(bboxes)
-          bboxes[:, [0,2]] = bboxes[:, [0, 2]]*nw/w + dw
-          bboxes[:, [1,3]] = bboxes[:, [1, 3]]*nh/h + dh
-          bboxes[:, 0:2][bboxes[:, 0:2] < 0] = 0
-          bboxes[:, 2][bboxes[:, 2] > iw] = iw
-          bboxes[:, 3][bboxes[:, 3] > ih] = ih
-          box_w = bboxes[:, 2] - bboxes[:, 0]
-          box_h = bboxes[:, 3] - bboxes[:, 1]
-          bboxes = bboxes[np.logical_and(box_w > 1, box_h > 1)]
+        box_data = np.zeros((self.max_boxes, 5))
+        if len(bboxes) > 0:
+            np.random.shuffle(bboxes)
+            if self.coords == "centroids":                
+                bboxes[..., 0] = bboxes[..., 0] * w
+                bboxes[..., 1] = bboxes[..., 1] * h
+                bboxes[..., 2] = bboxes[..., 2] * w
+                bboxes[..., 3] = bboxes[..., 3] * h
+                bboxes = coordinates_converter(bboxes, conversion="centroids2corners")
+            bboxes[:, [0,2]] = bboxes[:, [0, 2]]*nw/w + dw
+            bboxes[:, [1,3]] = bboxes[:, [1, 3]]*nh/h + dh
+            bboxes[:, 0:2][bboxes[:, 0:2] < 0] = 0
+            bboxes[:, 2][bboxes[:, 2] > iw] = iw
+            bboxes[:, 3][bboxes[:, 3] > ih] = ih
+            box_w = bboxes[:, 2] - bboxes[:, 0]
+            box_h = bboxes[:, 3] - bboxes[:, 1]
+            bboxes = bboxes[np.logical_and(box_w > 1, box_h > 1)]
+            if self.coords == "centroids":                
+                bboxes = coordinates_converter(bboxes, conversion="corners2centroids")
+                bboxes[..., 0] = bboxes[..., 0] / iw
+                bboxes[..., 1] = bboxes[..., 1] / ih
+                bboxes[..., 2] = bboxes[..., 2] / iw
+                bboxes[..., 3] = bboxes[..., 3] / ih
+            if len(bboxes) > self.max_boxes: 
+                bboxes = bboxes[:self.max_boxes]
 
-          if len(bboxes) > self.max_boxes: 
-              bboxes = bboxes[:self.max_boxes]
-
-          box_data[:len(bboxes)] = bboxes
-
-      return image_data, box_data
+            box_data[:len(bboxes)] = bboxes
+        return image_data, box_data
 
 if __name__ == "__main__":
     image_path = "/content/sample_data/voc_tiny/train/000288.jpg"
