d09b63dbf328729fcc6e1348598a8addd16ae8b4
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-03-22 11:05:34 +0700

    Update data_processing.py

--- data_utils/data_processing.py
+++ data_utils/data_processing.py
@@ -149,12 +149,9 @@ def preprocess_true_boxes(true_boxes, input_shape, anchors, anchors_mask, num_cl
     grid_shapes = [input_shape[:-1] // np.array(strides[i]) for i in range(len(strides))]
     y_true = [np.zeros((batch, grid_shapes[i][0], grid_shapes[i][1], len(anchors_mask[i]), 5 + num_classes), dtype='float32') for i in range(num_layers)]
 
-#     boxes_xy = (true_boxes[..., 0:2] + true_boxes[..., 2:4]) // 2
-#     boxes_wh =  true_boxes[..., 2:4] - true_boxes[..., 0:2]
-#     true_boxes[..., 0:2] = boxes_xy / input_shape[:-1]
-#     true_boxes[..., 2:4] = boxes_wh / input_shape[:-1]
     if coords == 'centroids':
-        boxes_wh   = true_boxes[..., 2:4] * input_shape[:-1][::-1]
+        boxes_xy   = true_boxes[..., 0:2]
+        boxes_wh   = true_boxes[..., 2:4]
     else:
         if coords == "corners":
             boxes_xy = (true_boxes[..., 0:2] + true_boxes[..., 2:4]) // 2       # Tính toán x_center, y_center
@@ -168,9 +165,9 @@ def preprocess_true_boxes(true_boxes, input_shape, anchors, anchors_mask, num_cl
             boxes_h = true_boxes[..., 3] - true_boxes[..., 2]                   # Tính toán h
             boxes_wh =  np.stack([boxes_w, boxes_h], axis=-1)
 
-        true_boxes[..., 0:2] = boxes_xy / input_shape[:-1][::-1]
-        true_boxes[..., 2:4] = boxes_wh / input_shape[:-1][::-1]
-        
+    true_boxes[..., 0:2] = boxes_xy / input_shape[:-1][::-1]
+    true_boxes[..., 2:4] = boxes_wh / input_shape[:-1][::-1]
+    
     anchors         = np.expand_dims(anchors, axis=0)
     anchor_maxes    = anchors / 2.
     anchor_mins     = -anchor_maxes
