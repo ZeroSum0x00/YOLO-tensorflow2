81598fec2048eccf343bd0e65f8fa288542688a3
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-03-24 09:27:27 +0700

    Update __init__.py

--- augmenter/__init__.py
+++ augmenter/__init__.py
@@ -3,7 +3,8 @@ from augmenter.geometric.flip import Flip, RandomFlip
 from augmenter.geometric.mosaic import Mosaic
 from augmenter.geometric.mixup import Mixup
 
-from augmenter.photometric.light_photometric_ops import Brightness, RandomBrightness
-from augmenter.photometric.light_photometric_ops import Saturation, RandomSaturation
-from augmenter.photometric.light_photometric_ops import Contrast, RandomContrast
-from augmenter.photometric.light_photometric_ops import Hue, RandomHue
+from augmenter.photometric.hue import Hue, RandomHue
+from augmenter.photometric.contrast import Contrast, RandomContrast
+from augmenter.photometric.brightness import Brightness, RandomBrightness
+from augmenter.photometric.staturation import Saturation, RandomSaturation
+from augmenter.photometric.light_photometric_ops import LightIntensityChange
