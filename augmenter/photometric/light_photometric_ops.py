b0ef11aac5acb97d7d8fb0727cc82f189b007068
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-03-25 10:27:55 +0700

    Update light_photometric_ops.py

--- augmenter/photometric/light_photometric_ops.py
+++ augmenter/photometric/light_photometric_ops.py
@@ -10,24 +10,24 @@ class LightIntensityChange:
         self.color_space = color_space
         
     def __call__(self, image, bboxes):
-      image         = np.array(image, np.uint8)
-      if self.color_space.lower() == "BGR":
-          image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-      elif self.color_space.lower() == "RGB":
-          image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-      r             = np.random.uniform(-1, 1, 3) * [self.hue, self.sat, self.val] + 1
-      hue, sat, val = cv2.split(image)
-      dtype         = image.dtype
-      x             = np.arange(0, 256, dtype=r.dtype)
-      lut_hue       = ((x * r[0]) % 180).astype(dtype)
-      lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
-      lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
-      image   = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val)))
-      if self.color_space.lower() == "BGR":
-          image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
-      elif self.color_space.lower() == "RGB":
-          image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
-      return image, bboxes
+        image         = np.array(image, np.uint8)
+        if self.color_space.lower() == "bgr":
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        elif self.color_space.lower() == "rgb":
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        r             = np.random.uniform(-1, 1, 3) * [self.hue, self.sat, self.val] + 1
+        hue, sat, val = cv2.split(image)
+        dtype         = image.dtype
+        x             = np.arange(0, 256, dtype=r.dtype)
+        lut_hue       = ((x * r[0]) % 180).astype(dtype)
+        lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
+        lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
+        image   = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val)))
+        if self.color_space.lower() == "bgr":
+            image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+        elif self.color_space.lower() == "rgb":
+            image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
+        return image, bboxes
 
 
 if __name__ == "__main__":
