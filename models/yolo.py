e8c6f804839ed7f15636e80e3a58062f18bb267c
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2024-04-26 13:13:02 +0700

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -46,7 +46,6 @@ class YOLO(tf.keras.Model):
     def train_step(self, data):
         loss_value = 0
         images, targets = data
-        targets = [targets[2], targets[1], targets[0]]
         
         with tf.GradientTape() as tape:
             y_pred = self.architecture(images, training=True)
@@ -80,7 +79,6 @@ class YOLO(tf.keras.Model):
     def test_step(self, data):
         loss_value = 0
         images, targets = data
-        targets = [targets[2], targets[1], targets[0]]
         y_pred  = self.architecture(images, training=False)
         
         for losses in self.loss_object:
